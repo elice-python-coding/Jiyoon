88f49f2024516f68bed2804aedaa0781ee81fb1a
moonjiyoon <ske0830@gmail.com>
2021-07-28 19:29:29 +0900

    14-1.py

---
+++ 14_SortingST/14-1.py
@@ -0,0 +1,17 @@
+'''
+국영수
+'''
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+array = []
+
+for i in range(n):
+    data = list(map(str, input().split()))
+    array.append([data[0], int(data[1]), int(data[2]), int(data[3])])
+
+array.sort(key = lambda x:(-x[1], x[2], -x[3], x[0]))
+
+for student in array:
+    print(student[0])
