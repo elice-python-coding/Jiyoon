7b8ed22e53ae671da4db5a4eb5c31f63daf61313
moonjiyoon <ske0830@gmail.com>
2021-10-27 01:53:09 +0900

    폴더명 변경

--- python_algorithm/String_operate/04.py
+++
@@ -1,9 +0,0 @@
-'''가장 흔한 단어'''
-import re
-import collections
-
-def mostCommonWord(paragraph, banned):
-    words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
-
-    counts = collections.Counter(words)
-    return counts.most_common(1)[0][0]
